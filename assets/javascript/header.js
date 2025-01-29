58eca3d80a9f61e1e32a5e4ec347f412b396a7b1
Milliesncl <Milliesncl@users.noreply.github.com>
2025-01-29 21:10:36 +0000

    deploy: 1644b9778b907fa7254bf29a4830cbbe744c6b23

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -2,6 +2,7 @@ const close = document.getElementById('close');
 const menu = document.getElementById('menu');
 const dialog = document.getElementById('dialog');
 const open = document.getElementById('open');
+const selection = document.getElementById('selection');
 
 close.addEventListener('click', () => {
   menu.classList.toggle('hidden');
@@ -11,4 +12,11 @@ close.addEventListener('click', () => {
 open.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   dialog.classList.toggle('hidden');
+});
+
+selection.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    menu.classList.add('hidden');
+    dialog.classList.add('hidden');
+  }
 });
